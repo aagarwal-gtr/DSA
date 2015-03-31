ee4f5d9830e66bd7f4286a2ddbb3925482260666
Abhinav Agarwal <aagarwal.gtr@gmail.com>
2015-03-31 20:55:17 +0530

    add printbonuspolicemen function

--- Lab 8 - BST, Hashing/printBonusPolicemen.c
+++ Lab 8 - BST, Hashing/printBonusPolicemen.c
@@ -18,5 +18,25 @@ static inline int getMaxamount(PoliceTree root)
 
 void printBonusPolicemen(PoliceTree root)
 {
+	FILE *fp;
+	int i;
+	PoliceTree curr;
+	fp=fopen("bonus.txt", "w+");
+	if(fp == NULL)
+	{
+			fprintf(stderr, "\nCannot open bonus file for writing\n");
+			exit(-1);
+	}
+	i = 0.9 * getMaxamount(root);
+	curr = root;
+	while(curr!=NULL)
+	{
+		if(curr->amount >= i) {
+			fprintf(fp,"%d %d\n",curr->id, curr->amount);
+		}
+		curr = curr->succ;
+	}
+
+	fclose(fp);
 
 }
